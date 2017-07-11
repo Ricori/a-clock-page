0add11ec7f775974beb591ee77cd4bdf9388464d
Junhan Cao <17014@163.com>
2017-07-11 22:25:13 +0800

    updata

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,27 @@
 require('../scss/main.scss');
-var mainRender = require('../templates/main.ejs')
 
-/* 
+var maintp = require('../templates/main.html');
+var Shifter = require('./Shifter');
 
-Your JS code
+window.onload = function(){ 
+    document.body.innerHTML = maintp;
+    Shifter.init();
+    startTime();
+    //Shifter.Shape.switchShape(Shifter.ShapeBuilder.letter("23:33:33"));
+};
 
-*/
-
-document.body.innerHTML = mainRender({data:'This is a webpack-demo'});
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    if(m < 10) m = "0" + m;
+    if(s < 10) s = "0" + s;
+    var day_or_night = (h > 11) ? "PM": "AM";
+    if (h > 12) h -= 12;
+    var timetext = h + ":" + m + ":" + s + " " + day_or_night;
+    Shifter.Shape.switchShape(Shifter.ShapeBuilder.letter(timetext));
+    setTimeout(function(){
+        startTime();
+    },5000);
+}
